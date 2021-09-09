7fca9db51d9cbddb76c630d79fd96e73550ff58c
Duc Ta <tminhduc2811@gmail.com>
2021-09-09 23:15:49 +0700

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,9 +22,9 @@ const responsive = {
 
 const HomePage = () => {
   return (
-    <>      
+    <>
       <section className="hero-section">
-        <div className="container relative py-28 sm:py-36 mx-auto px-5 lg:px-40">
+        <div className="container py-28 sm:py-36 mx-auto px-5 lg:px-40">
           <div className='lg:w-3/4 text-center sm:text-left'>
             <p className='text-xl text-white'>👋 Hi there. I'm Duc</p>
             <p className='text-white text-2xl pt-5 font-medium sm:text-4xl'>I'm working as a <span className='text-yellow font-semibold'>full-stack</span> developer. I like to create amazing stuff with web technologies.</p>
@@ -32,27 +32,43 @@ const HomePage = () => {
           </div>
         </div>
       </section>
-      <section>
-        <div className="container mx-auto relative pt-5 lg:px-40">
-          <div className="text-center">
-            <h2 className='text-white text-2xl my-8 font-medium sm:text-4xl'>Familiar with</h2>
-            <Carousel responsive={responsive} renderDotsOutside ssr>
-              <Image className='card-img' src="/images/js.png" height='100' width='100' alt="javascript" />
-              <Image className='card-img' src="/images/ts.png" height='100' width='100' alt="typescript" />
-              <Image className='card-img' src="/images/go.png" height='100' width='100' alt="golang" />
-              <Image className='card-img' src="/images/java.png" height='100' width='100' alt="java" />
-              <Image className='card-img' src="/images/react.png" height='100' width='100' alt="reactjs" />
-              <Image className='card-img' src="/images/vue.png" height='100' width='100' alt="vuejs" />
-              <Image className='card-img' src="/images/spring.jpg" height='100' width='100' alt="spring boot" />
-            </Carousel>
+      <section className="container mx-auto pt-5 lg:px-40">
+        <div className="text-center">
+          <h2 className='text-white text-2xl my-8 font-medium sm:text-4xl pb-5'>Familiar with</h2>
+          <Carousel responsive={responsive} renderDotsOutside ssr>
+            <Image className='card-img' src="/images/js.png" height='100' width='100' alt="javascript" />
+            <Image className='card-img' src="/images/ts.png" height='100' width='100' alt="typescript" />
+            <Image className='card-img' src="/images/go.png" height='100' width='100' alt="golang" />
+            <Image className='card-img' src="/images/java.png" height='100' width='100' alt="java" />
+            <Image className='card-img' src="/images/react.png" height='100' width='100' alt="reactjs" />
+            <Image className='card-img' src="/images/vue.png" height='100' width='100' alt="vuejs" />
+            <Image className='card-img' src="/images/spring.jpg" height='100' width='100' alt="spring boot" />
+          </Carousel>
+        </div>
+      </section>
+      <section className='container mx-auto pt-5 lg:px-40 text-center'>
+        <h2 className='text-white text-2xl mt-8 font-medium sm:text-4xl'>What I do</h2>
+        <div className="md:flex">
+          <div>
+            <Image src='/images/website-illustration.svg' width='440' height='300' />
           </div>
+          <div className="flex-1 w-full md:text-left md:py-20">
+            <p className="text-white text-lg p-3 md:text-xl">Well, mostly everything related to <span className='text-yellow'>Web Development</span>. Btw, I'm usually open for freelance, remote, or part-time jobs, so don't hesitate to contact me if you're interested in my work.</p>
+          </div>
+        </div>
+      </section>
+      <section className='hero-section text-center pt-4 pb-16'>
+        <h2 className='text-white text-2xl mt-8 font-medium sm:text-4xl'>Find me on</h2>
+        <div className="flex flex-wrap justify-center mt-2">
+        {/* <button className="bg-grey text-xl mt-5 px-5 py-2 rounded-xl hover:bg-yellow transition-colors duration-300">Github</button>
+        <button className="bg-grey text-xl mt-5 px-5 py-2 rounded-xl hover:bg-yellow transition-colors duration-300">LinkedIn</button> */}
         </div>
       </section>
       <section>
         <div className="container mx-auto relative pt-5 lg:px-40 text-center">
           <h2 className='text-white text-2xl my-8 font-medium sm:text-4xl'>The site is under development and will be finished soon...</h2>
           <p className='text-white text-xl'>The source code can be found at
-          <ExternalLink className='pl-1 text-yellow' href="https://github.com/tminhduc2811/me-myself-n-i" rel='nofollow'>HERE</ExternalLink> 
+            <ExternalLink className='pl-1 text-yellow' href="https://github.com/tminhduc2811/me-myself-n-i" rel='nofollow'>HERE</ExternalLink>
           </p>
         </div>
       </section>
