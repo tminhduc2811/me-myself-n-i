123123ed21d4e7f0c2aca1368393aff5fcb6c379
Duc Ta <tminhduc2811@gmail.com>
2021-09-27 11:49:08 +0700

    add blur placeholder for post image

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,15 +2,23 @@ import Image from 'next/image'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 
+export interface PostInterface {
+  image: string
+  base64: string
+  href: string
+  title: string
+  description: string
+}
+
 const Post = ({ meta }) => {
 
-  const { href, title, description, image } = meta
+  const { href, title, description, image, base64 } = meta
 
   return (
     <div className="flex flex-col bg-[#151A22] rounded-xl shadow-xl mt-4 mx-2 hover:bg-[#212529] transition-colors duration-300 lg:flex-row overflow-hidden">
       <Link href={href} passHref>
         <a className="h-52 w-auto lg:w-4/5 xl:w-1/3 lg:h-auto relative">
-          <Image className='inset-0 object-cover object-center' src={image} alt={title} layout='fill' />
+          <Image className='inset-0 object-cover object-center' src={image} alt={title} layout='fill' placeholder='blur' blurDataURL={base64} />
         </a>
       </Link>
       <div className="w-full py-4 px-3 text-grey flex flex-col justify-between ">
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 import fs from 'fs'
 import path from 'path'
 import { POSTS_PATH } from '../../constants'
 import matter from 'gray-matter'
-import Post from '../../components/Post'
+import Post, { PostInterface } from '../../components/Post'
 
 const PostsPage = ({ blogs }) => {
   return (
@@ -28,14 +28,31 @@ export const getStaticProps = async () => {
         'utf-8'
       )
 
-      const { data } = matter(markdown)
+      const { data } = matter(markdown) as unknown as { data: PostInterface }
+
+      // process images, generate blur data url
+      getPlaiceholder(
+        data.image,
+        { size: 10 }
+      ).then(({ base64 }) => {
+        data.base64 = base64
+      })
+
       return {
         ...data,
         href: `/blog/${fileName.replace(/\.md?$/, '')}`
       }
     })
 
-  // 
+
+  blogs.forEach(async blog => {
+    const { base64 } = await getPlaiceholder(
+      blog.image,
+      { size: 10 }
+    )
+    blog.base64 = base64
+  })
+
   return {
     props: {
       blogs
